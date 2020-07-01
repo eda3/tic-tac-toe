9775bb957355b1d8497af78751d1f4164de5e149
eda3 <gerorin1010@gmail.com>
2020-07-01 19:26:10 +0900

    feat: 記法 -> Boardオブジェクトに変換するfrom_notation()を追加

--- src/board.py
+++ src/board.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import re
 from re import Pattern
 
@@ -8,6 +10,14 @@ class Board:
             raise Exception("Boardクラスの初期化は9文字でなければなりません。")
         self._squares_string = squares_string
 
+    @property
+    def squares_string(self):
+        pass
+
+    @squares_string.getter
+    def squares_string(self):
+        return self._squares_string
+
     @staticmethod
     def is_valid_notation(notation: str) -> bool:
         """記法の妥当性チェック"""
@@ -15,3 +25,14 @@ class Board:
         pattern: Pattern = re.compile(match_str)
 
         return True if pattern.match(notation) else False
+
+    @staticmethod
+    def from_notation(notation: str) -> Board:
+        """ 記法 -> Boardオブジェクトの変換"""
+        if Board.is_valid_notation(notation) is False:
+            raise Exception("記法が正しくありません")
+
+        # 数値部分だけを抜き取る
+        squares_string: str = (notation[1:4] + notation[5:8] + notation[9:12])
+
+        return Board(squares_string)
--- tests/test_board.py
+++ tests/test_board.py
@@ -8,3 +8,15 @@ def test_is_valid_notation():
     board = Board("123456789")
     assert board.is_valid_notation("[123,456,789]") is True
     assert board.is_valid_notation("aaa") is False
+
+
+def test_from_notation():
+    board = Board("123456789")
+
+    # 引数が正しくない場合、例外が発生する
+    with pytest.raises(Exception):
+        board.from_notation("aaa")
+
+    # 引数が正しい場合、数値部分を含むBoardインスタンスが返却される
+    new_board = board.from_notation("[123,456,789]")
+    assert new_board.squares_string == "123456789"
