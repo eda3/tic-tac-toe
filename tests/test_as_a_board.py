6c513cef12c39d131ca0df8cc1fb5670ab230f7d
eda3 <gerorin1010@gmail.com>
2020-07-07 21:27:02 +0900

    test: display_board()のテストを実装

--- tests/test_as_a_board.py
+++ tests/test_as_a_board.py
@@ -1,1 +1,21 @@
 import pytest
+import as_a_board
+from board import Board
+from gamestate import GameState
+
+
+def test_display_board():
+    game_state: GameState = GameState.initial()
+    return_text: str = as_a_board.display_board(game_state.board)
+    initial_text: str = (
+        "  1 2 3\n"
+        " ┏━┳━┳━┓\n"
+        "a┃ ┃ ┃ ┃\n"
+        " ┣━╋━╋━┫\n"
+        "b┃ ┃ ┃ ┃\n"
+        " ┣━╋━╋━┫\n"
+        "c┃ ┃ ┃ ┃\n"
+        " ┗━┻━┻━┛\n"
+    )
+
+    assert return_text == initial_text
